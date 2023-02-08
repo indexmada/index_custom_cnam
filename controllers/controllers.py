34a1e9dfd5cace4a8a372288f8c5b10b64a89130
Mizara983 <pounasatu@gmail.com>
2023-02-08 21:57:58 +0300

    Attestation de réussite dans portal view

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import OrderedDict
+
+from odoo import http, _
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+from odoo.http import request
+from odoo.exceptions import AccessError, MissingError
 
 
-class IndexCustomCnam(http.Controller):
+class IndexCustomCnam(CustomerPortal):
     # @http.route('/index_custom_cnam/update_cost', auth='public')
     # def index(self, **kw):
     #     costs = http.request.env['unit.enseigne.config.cost'].sudo().search([])
@@ -30,3 +36,59 @@ class IndexCustomCnam(http.Controller):
                 })
 
         return "Dates updated"
+
+    @http.route(['/my/cnam_documents'], type='http', auth="user", website=True)
+    def portal_cnam_documents(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, **kw):
+
+        values = self._prepare_portal_layout_values()
+        note_obj = request.env['note.list'].sudo()
+
+        searchbar_filters = {
+            'all': {'label': _('All'), 'domain': []},
+        }
+
+        if not filterby:
+            filterby = 'all'
+        domain = searchbar_filters.get(filterby, searchbar_filters.get('all'))['domain']
+
+        archive_groups = self._get_archive_groups('note.list', domain) if values.get('my_details') else []
+        if date_begin and date_end:
+            domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        domain += [('partner_id', '=', request.env.user.partner_id.id)]
+
+        domain += [('validation', '=', 'validated')]
+        # count for pager
+        note_count = note_obj.search_count(domain)
+        # pager
+        pager = portal_pager(
+            url="/my/note",
+            url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby,},
+            total=note_count,
+            page=page,
+            step=self._items_per_page
+        )
+        # content according to pager and archive selected
+        note_ids = note_obj.search(domain, order=None, limit=self._items_per_page, offset=pager['offset'])
+        attestation_ok = []
+        group_att = request.env['note.list'].sudo()
+        for note in note_ids:
+            vals_att = {'etudiant': note.partner_id.id,
+                        'as': note.note_list_filter_id.year.id
+                        }
+            if vals_att not in attestation_ok:
+                attestation_ok.append(vals_att)
+                group_att |= note
+
+
+        values.update({
+            'date': date_begin,
+            'note_ids': group_att,
+            'page_name': 'Note',
+            'pager': pager,
+            'archive_groups': archive_groups,
+            'default_url': '/my/note',
+            'sortby': sortby,
+            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+        })
+        return request.render("index_custom_cnam.portal_cnam_documents", values)
