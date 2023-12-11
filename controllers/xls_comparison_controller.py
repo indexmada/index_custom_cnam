52cdd22cfdc465f8423e9ce7a8f75fc36c1ef1f0
Mizara983 <pounasatu@gmail.com>
2023-12-11 16:44:13 +0300

    oiugd

--- controllers/xls_comparison_controller.py
+++ controllers/xls_comparison_controller.py
@@ -30,8 +30,7 @@ class xlsComparisonController(http.Controller):
 
 
     def report_excel_xls_comparison(self, workbook, school_year,str_sem):
-        worksheet_ost = workbook.add_worksheet("DNS")
-        self.style(worksheet_ost)
+        tab = ['monthly', 'weekly']
         bold_center_11 = workbook.add_format({
             'align': 'center',
             'valign': 'vcenter',
@@ -66,87 +65,108 @@ class xlsComparisonController(http.Controller):
             "font_size": 11,
             "bold": False
         })
-        year_id = request.env['school.year'].sudo().browse(int(school_year))
+        row_tab = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 'J', 'K', 'L', "M", "N", "O", "P"]
+        for t in tab:
+            sheet_name = "Par mois" if t == "monthly" else "Par Semaine"
+            worksheet_ost = workbook.add_worksheet(sheet_name)
+            if t == 'monthly':
+                self.style_month(worksheet_ost)
+            else:
+                self.style(worksheet_ost)
 
-        year = year_id.name
+            year_id = request.env['school.year'].sudo().browse(int(school_year))
 
-        school_year_ids = self.get_school_years(year,year_id)
+            year = year_id.name
 
-        worksheet_ost.merge_range('C1:G1','TABLEAU DE COMPARAISON - RECAP INSCRIPTION '+str(year),bold_center_11)
+            school_year_ids = self.get_school_years(year,year_id)
 
-        center_ids = request.env['region.center'].sudo().search([])
-        row = 4
-        for center in center_ids:
-            insc_domain = [('region_center_id', '=', center.id), ('type', '=', 'registration')]
-            reinsc_domain = [('region_center_id', '=', center.id), ('type', '=', 're-registration')]
+            worksheet_ost.merge_range('B1:G1','TABLEAU DE COMPARAISON - RECAP INSCRIPTION '+str(year),bold_center_11)
 
-            insc_ue_ids = request.env['inscription.edu'].sudo().search(insc_domain)
-            reinsc_ue_ids = request.env['inscription.edu'].sudo().search(reinsc_domain)
+            center_ids = request.env['region.center'].sudo().search([])
+            row = 4
+            for center in center_ids:
+                insc_domain = [('region_center_id', '=', center.id), ('type', '=', 'registration')]
+                reinsc_domain = [('region_center_id', '=', center.id), ('type', '=', 're-registration')]
 
-            insc_ue_ids_temp = request.env['inscription.edu'].sudo()
-            reinsc_ue_ids_temp = request.env['inscription.edu'].sudo()
+                insc_ue_ids = request.env['inscription.edu'].sudo().search(insc_domain)
+                reinsc_ue_ids = request.env['inscription.edu'].sudo().search(reinsc_domain)
 
-            if str_sem and str_sem != '':
-                sem_ids = str_sem.split('-')
-                for i in sem_ids:
-                    # for insc in insc_ue_ids:
-                    #     print('*'*20)
-                    #     print(i,' ---- ', insc.get_semester_insc().ids)
-                    insc_ue_ids_temp |= insc_ue_ids.filtered(lambda insc: int(i) in insc.get_semester_insc().ids)
-                    reinsc_ue_ids_temp |= reinsc_ue_ids.filtered(lambda re: int(i) in re.get_semester_insc().ids)
+                insc_ue_ids_temp = request.env['inscription.edu'].sudo()
+                reinsc_ue_ids_temp = request.env['inscription.edu'].sudo()
 
-                insc_ue_ids = insc_ue_ids_temp
-                reinsc_ue_ids = reinsc_ue_ids_temp
+                if str_sem and str_sem != '':
+                    sem_ids = str_sem.split('-')
+                    for i in sem_ids:
+                        # for insc in insc_ue_ids:
+                        #     print('*'*20)
+                        #     print(i,' ---- ', insc.get_semester_insc().ids)
+                        insc_ue_ids_temp |= insc_ue_ids.filtered(lambda insc: int(i) in insc.get_semester_insc().ids)
+                        reinsc_ue_ids_temp |= reinsc_ue_ids.filtered(lambda re: int(i) in re.get_semester_insc().ids)
 
-            if insc_ue_ids or reinsc_ue_ids:
-                cell = 'C'+str(row)+':K'+str(row)
-                worksheet_ost.merge_range(cell, center.name, cell_bold_center_11)
+                    insc_ue_ids = insc_ue_ids_temp
+                    reinsc_ue_ids = reinsc_ue_ids_temp
 
-                row +=1
-                if len(school_year_ids)>0:
-                    cell = 'C'+str(row)+':E'+str(row)
-                    worksheet_ost.merge_range(cell, str(school_year_ids[0][0])+'-'+str(school_year_ids[0][1]), cell_bold_center_11)
+                if insc_ue_ids or reinsc_ue_ids:
+                    col1 = 1 if t == "monthly" else 2
+                    col2 = 9 if t == "monthly" else 10
+                    cell = row_tab[col1]+str(row)+':'+row_tab[col2]+str(row)
+                    worksheet_ost.merge_range(cell, center.name, cell_bold_center_11)
 
-                    cell = 'C'+str(row+1)
-                    worksheet_ost.write(cell, 'REINS', cell_center_11)
-                    cell = 'D'+str(row+1)
-                    worksheet_ost.write(cell, 'NVX', cell_center_11)
-                    cell = 'E'+str(row+1)
-                    worksheet_ost.write(cell, 'UE Vendues', cell_center_11)
+                    row +=1
+                    if len(school_year_ids)>0:
+                        cell = row_tab[col1]+str(row)+':'+row_tab[col1+2]+str(row)
+                        worksheet_ost.merge_range(cell, str(school_year_ids[0][0])+'-'+str(school_year_ids[0][1]), cell_bold_center_11)
 
-                if len(school_year_ids)>1:
-                    cell = 'F'+str(row)+':H'+str(row)
-                    worksheet_ost.merge_range(cell, str(school_year_ids[1][0])+'-'+str(school_year_ids[1][1]), cell_bold_center_11)
+                        cell = row_tab[col1]+str(row+1)
+                        worksheet_ost.write(cell, 'REINS', cell_center_11)
+                        cell = row_tab[col1+1]+str(row+1)
+                        worksheet_ost.write(cell, 'NVX', cell_center_11)
+                        cell = row_tab[col1+2]+str(row+1)
+                        worksheet_ost.write(cell, 'UE Vendues', cell_center_11)
 
-                    cell = 'F'+str(row+1)
-                    worksheet_ost.write(cell, 'REINS', cell_center_11)
-                    cell = 'G'+str(row+1)
-                    worksheet_ost.write(cell, 'NVX', cell_center_11)
-                    cell = 'H'+str(row+1)
-                    worksheet_ost.write(cell, 'UE Vendues', cell_center_11)
+                    if len(school_year_ids)>1:
+                        cell = row_tab[col1+3]+str(row)+':'+row_tab[col1+5]+str(row)
+                        worksheet_ost.merge_range(cell, str(school_year_ids[1][0])+'-'+str(school_year_ids[1][1]), cell_bold_center_11)
 
-                if len(school_year_ids)>2:
-                    cell = 'I'+str(row)+':K'+str(row)
-                    worksheet_ost.merge_range(cell, str(school_year_ids[2][0])+'-'+str(school_year_ids[2][1]), cell_bold_center_11)
+                        cell = row_tab[col1+3]+str(row+1)
+                        worksheet_ost.write(cell, 'REINS', cell_center_11)
+                        cell = row_tab[col1+4]+str(row+1)
+                        worksheet_ost.write(cell, 'NVX', cell_center_11)
+                        cell = row_tab[col1+5]+str(row+1)
+                        worksheet_ost.write(cell, 'UE Vendues', cell_center_11)
 
-                    cell = 'I'+str(row+1)
-                    worksheet_ost.write(cell, 'REINS', cell_center_11)
-                    cell = 'J'+str(row+1)
-                    worksheet_ost.write(cell, 'NVX', cell_center_11)
-                    cell = 'K'+str(row+1)
-                    worksheet_ost.write(cell, 'UE Vendues', cell_center_11)
+                    if len(school_year_ids)>2:
+                        cell = row_tab[col1+6]+str(row)+':'+row_tab[col1+8]+str(row)
+                        worksheet_ost.merge_range(cell, str(school_year_ids[2][0])+'-'+str(school_year_ids[2][1]), cell_bold_center_11)
 
+                        cell = row_tab[col1+6]+str(row+1)
+                        worksheet_ost.write(cell, 'REINS', cell_center_11)
+                        cell = row_tab[col1+7]+str(row+1)
+                        worksheet_ost.write(cell, 'NVX', cell_center_11)
+                        cell = row_tab[col1+8]+str(row+1)
+                        worksheet_ost.write(cell, 'UE Vendues', cell_center_11)
 
-                self.fillDateCell(worksheet_ost, row +1, cell_bold_center_11)
-                self.fillUeData(worksheet_ost, row+2, cell_center_11,school_year_ids,reinsc_ue_ids, insc_ue_ids)
+                    monthly = True if t == "monthly" else False
 
-                row += 104
+                    self.fillDateCell(worksheet_ost, row +1, cell_bold_center_11, monthly)
+                    self.fillUeData(worksheet_ost, row+2, cell_center_11,school_year_ids,reinsc_ue_ids, insc_ue_ids, monthly)
 
-    def fillUeData(self, worksheet_ost, row, row_style, year_ids, reinsc_ids, insc_ids):
+                    if t == "monthly":
+                        row += 32
+                    else:
+                        row += 104
+
+    def fillUeData(self, worksheet_ost, row, row_style, year_ids, reinsc_ids, insc_ids, monthly):
         counter = 0
-        col = ['C', 'F', 'I']
-        col_nvx = ['D', 'G', 'J']
-        col_ue = ['E', 'H', 'K']
+        if monthly:
+            col = ["B", "E", "H"]
+            col_nvx = ["C", "F", "I"]
+            col_ue = ["D", "G", "J"]
+
+        else:
+            col = ['C', 'F', 'I']
+            col_nvx = ['D', 'G', 'J']
+            col_ue = ['E', 'H', 'K']
 
         for year_id in year_ids:
             year_tab = year_id
@@ -159,12 +179,13 @@ class xlsComparisonController(http.Controller):
             for m in month_tab_1:
                 if m in [9,11]:
                     day_tab = [(1,7), (8,15), (16,22), (23,30)]
+                    month_end = 30
                 else:
                     day_tab = [(1,7), (8,15), (16,22), (23,31)]
-
-                for day in day_tab:
-                    date_begin = date(int(year_tab[0]), m, day[0])
-                    date_end = date(int(year_tab[0]), m, day[1])
+                    month_end = 31
+                if monthly:
+                    date_begin = date(int(year_tab[0]), m, 1)
+                    date_end = date(int(year_tab[0]), m, month_end)
                     reinsc_filtered = reinsc_ids.filtered(lambda re: re.date >= date_begin and re.date <= date_end)
                     insc_filtered = insc_ids.filtered(lambda insc: insc.date >= date_begin and insc.date <= date_end)
                     cell = col[counter]+str(r)
@@ -186,18 +207,46 @@ class xlsComparisonController(http.Controller):
                     total_ue += nb_ue
 
                     r+= 2
+                else:
+                    for day in day_tab:
+                        date_begin = date(int(year_tab[0]), m, day[0])
+                        date_end = date(int(year_tab[0]), m, day[1])
+                        reinsc_filtered = reinsc_ids.filtered(lambda re: re.date >= date_begin and re.date <= date_end)
+                        insc_filtered = insc_ids.filtered(lambda insc: insc.date >= date_begin and insc.date <= date_end)
+                        cell = col[counter]+str(r)
+                        cell_nvx = col_nvx[counter]+str(r)
+                        worksheet_ost.write(cell, len(reinsc_filtered), row_style)
+                        worksheet_ost.write(cell_nvx, len(insc_filtered), row_style)
+
+                        cell_tot = col[counter]+str(r+1)+':'+col_nvx[counter]+str(r+1)
+                        worksheet_ost.merge_range(cell_tot, len(reinsc_filtered)+len(insc_filtered), row_style)
+
+                        # UE Vendues
+                        nb_ue = len(reinsc_filtered.mapped('units_enseignes')) + len(reinsc_filtered.mapped('other_ue_ids')) + len(insc_filtered.mapped('units_enseignes')) + len(insc_filtered.mapped('other_ue_ids'))
+                        cell_ue = col_ue[counter]+str(r)+':'+col_ue[counter]+str(r+1)
+                        worksheet_ost.merge_range(cell_ue, str(nb_ue), row_style)
+
+                        # Increase Total
+                        total_reinsc += len(reinsc_filtered)
+                        total_insc += len(insc_filtered)
+                        total_ue += nb_ue
+
+                        r+= 2
 
             for m in month_tab_2:
                 if m in [4,6]:
                     day_tab = [(1,7), (8,15), (16,22), (23,30)]
+                    month_end = 30
                 elif m == 2:
                     day_tab = [(1,7), (8,15), (16,22), (23,28)]
+                    month_end = 28
                 else:
                     day_tab = [(1,7), (8,15), (16,22), (23,31)]
+                    month_end = 31
 
-                for day in day_tab:
-                    date_begin = date(int(year_tab[1]), m, day[0])
-                    date_end = date(int(year_tab[1]), m, day[1])
+                if monthly: 
+                    date_begin = date(int(year_tab[1]), m, 1)
+                    date_end = date(int(year_tab[1]), m, month_end)
                     reinsc_filtered = reinsc_ids.filtered(lambda re: re.date >= date_begin and re.date <= date_end)
                     insc_filtered = insc_ids.filtered(lambda insc: insc.date >= date_begin and insc.date <= date_end)
                     cell = col[counter]+str(r)
@@ -219,6 +268,32 @@ class xlsComparisonController(http.Controller):
                     total_ue += nb_ue
 
                     r+= 2
+
+                else:
+                    for day in day_tab:
+                        date_begin = date(int(year_tab[1]), m, day[0])
+                        date_end = date(int(year_tab[1]), m, day[1])
+                        reinsc_filtered = reinsc_ids.filtered(lambda re: re.date >= date_begin and re.date <= date_end)
+                        insc_filtered = insc_ids.filtered(lambda insc: insc.date >= date_begin and insc.date <= date_end)
+                        cell = col[counter]+str(r)
+                        cell_nvx = col_nvx[counter]+str(r)
+                        worksheet_ost.write(cell, len(reinsc_filtered), row_style)
+                        worksheet_ost.write(cell_nvx, len(insc_filtered), row_style)
+
+                        cell_tot = col[counter]+str(r+1)+':'+col_nvx[counter]+str(r+1)
+                        worksheet_ost.merge_range(cell_tot, len(reinsc_filtered)+len(insc_filtered), row_style)
+
+                        # UE Vendues
+                        nb_ue = len(reinsc_filtered.mapped('units_enseignes')) + len(reinsc_filtered.mapped('other_ue_ids')) + len(insc_filtered.mapped('units_enseignes')) + len(insc_filtered.mapped('other_ue_ids'))
+                        cell_ue = col_ue[counter]+str(r)+':'+col_ue[counter]+str(r+1)
+                        worksheet_ost.merge_range(cell_ue, str(nb_ue), row_style)
+
+                        # Increase Total
+                        total_reinsc += len(reinsc_filtered)
+                        total_insc += len(insc_filtered)
+                        total_ue += nb_ue
+
+                        r+= 2
 
             # Total
             # Reinsc
@@ -240,31 +315,41 @@ class xlsComparisonController(http.Controller):
             counter +=1
 
 
-    def fillDateCell(self,worksheet_ost, row, row_style):
+    def fillDateCell(self,worksheet_ost, row, row_style, monthly = False):
         cell = 'A'+str(row)
         worksheet_ost.write(cell, 'MOIS', row_style)
-        cell = 'B'+str(row)
-        worksheet_ost.write(cell, 'DATE', row_style)
+        if not monthly:
+            cell = 'B'+str(row)
+            worksheet_ost.write(cell, 'DATE', row_style)
 
         months = ['JUILLET', 'AOUT', 'SEPTEMBRE', 'OCTOBRE', 'NOVEMBRE', 'DECEMBRE', 'JANVIER', 'FEVRIER','MARS', 'AVRIL', 'MAI','JUIN']
 
         row += 1
 
         for month in months:
-            cell = 'A'+str(row)+':A'+str(row+7)
+            if monthly:
+                cell = 'A'+str(row)+':A'+str(row+1)
+            else:
+                cell = 'A'+str(row)+':A'+str(row+7)
             worksheet_ost.merge_range(cell, month, row_style)
 
-            cell = 'B'+str(row)+':B'+str(row+1)
-            worksheet_ost.merge_range(cell, "01er Au 07",row_style)
-            cell = 'B'+str(row+2)+':B'+str(row+3)
-            worksheet_ost.merge_range(cell, "08 Au 15",row_style)
-            cell = 'B'+str(row+4)+':B'+str(row+5)
-            worksheet_ost.merge_range(cell, "16 Au 22",row_style)
-            cell = 'B'+str(row+6)+':B'+str(row+7)
-            worksheet_ost.merge_range(cell, "23 Au 30",row_style)
+            if monthly:
+                row += 2
+            else:
+                cell = 'B'+str(row)+':B'+str(row+1)
+                worksheet_ost.merge_range(cell, "01er Au 07",row_style)
+                cell = 'B'+str(row+2)+':B'+str(row+3)
+                worksheet_ost.merge_range(cell, "08 Au 15",row_style)
+                cell = 'B'+str(row+4)+':B'+str(row+5)
+                worksheet_ost.merge_range(cell, "16 Au 22",row_style)
+                cell = 'B'+str(row+6)+':B'+str(row+7)
+                worksheet_ost.merge_range(cell, "23 Au 30",row_style)
 
-            row += 8
-        cell = 'A'+str(row)+':B'+str(row+1)
+                row += 8
+        if monthly:
+            cell = 'A'+str(row)+':A'+str(row+1)
+        else:
+            cell = 'A'+str(row)+':B'+str(row+1)
         worksheet_ost.merge_range(cell, 'TOTAL', row_style)
 
 
@@ -272,6 +357,12 @@ class xlsComparisonController(http.Controller):
         worksheet.set_column('A:A', 17)
         worksheet.set_column('B:B', 13)
         worksheet.set_column('C:K', 11)
+        for i in range(1,300):
+            worksheet.set_row(i, 18)
+
+    def style_month(self, worksheet):
+        worksheet.set_column('A:A', 17)
+        worksheet.set_column('B:K', 11)
         for i in range(1,300):
             worksheet.set_row(i, 18)
 
