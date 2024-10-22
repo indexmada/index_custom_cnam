840b2839e9fafcc36e34a825a2146a419f2e720e
Lucka <60772774+Luckaheriniaina@users.noreply.github.com>
2024-10-22 09:18:13 +0300

    Update action_manager.js

--- static/src/js/action_manager.js
+++ static/src/js/action_manager.js
@@ -1,29 +1,25 @@
-odoo.define('index_custom_cnam.action_manager', function (require) {
-    "use strict";
+odoo.define('module_name.your_class', function (require) {
+    'use strict';
 
-    var ActionManager = require('web.ActionManager');
-    var framework = require('web.framework');
-    var session = require('web.session');
+    var AbstractAction = require('web.AbstractAction');
+    var core = require('web.core');
 
-    ActionManager.include({
-        _executexlsxReportDownloadAction: function (action) {
-            console.log("Executing XLSX Report Download Action:", action); // Log l'action
-            framework.blockUI();
-            var def = $.Deferred();
-            session.get_file({
-                url: '/xlsx_reports',
-                data: action.data,
-                success: def.resolve.bind(def),
-                error: (error) => this.call('crash_manager', 'rpc_error', error),
-                complete: framework.unblockUI,
-            });
-            return def;
-        },
+    var YourClass = AbstractAction.extend({
         _handleAction: function (action, options) {
-            if (action.type === 'ir_actions_xlsx_download') {
+            // Vérification de l'action avant d'accéder à ses propriétés
+            if (action && action.type === 'ir_actions_xlsx_download') {
                 return this._executexlsxReportDownloadAction(action, options);
             }
             return this._super.apply(this, arguments);
         },
+
+        _executexlsxReportDownloadAction: function (action, options) {
+            // Logique pour exécuter l'action de téléchargement XLSX
+            // Ajoutez ici votre code pour gérer le téléchargement
+            console.log('Téléchargement en cours pour l\'action:', action);
+            // Ajoutez ici la logique de téléchargement
+        },
     });
+
+    core.action_registry.add('module_name.your_class', YourClass);
 });
